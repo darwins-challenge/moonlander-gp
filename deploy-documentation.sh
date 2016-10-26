129ccd4d13c354468c8d7217a3d7fc26decb01af
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2016-10-26 21:18:51 +0200

    Push to correct repository

--- deploy-documentation.sh
+++ deploy-documentation.sh
@@ -9,13 +9,13 @@ fi
 rev=$(git rev-parse --short HEAD)
 
 cd target/doc
-echo '<meta http-equiv=refresh content=0;url=/moonlander-ast-rust/ast/index.html>' > index.html
+echo '<meta http-equiv=refresh content=0;url=/moonlander-gp/ast/index.html>' > index.html
 
 git init
 git config user.name "Daan van Berkel"
 git config user.email "daan@fifth-postulate.nl"
 
-git remote add upstream "https://$GH_TOKEN@github.com/darwins-challenge/moonlander-ast-rust.git"
+git remote add upstream "https://$GH_TOKEN@github.com/darwins-challenge/moonlander-gp.git"
 git fetch upstream
 git reset upstream/gh-pages
 
