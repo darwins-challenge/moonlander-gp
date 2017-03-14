67e2e22dffe1cd1fcea5172e66a5e06a3de22245
Daan van Berkel <daan@fifth-postulate.nl>
2017-03-14 20:43:44 +0000

    rebuild pages at 19e42c3

--- implementors/rustc_serialize/serialize/trait.Encodable.js
+++ implementors/rustc_serialize/serialize/trait.Encodable.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors["moonlander_gp"] = ["impl <a class='trait' href='rustc_serialize/serialize/trait.Encodable.html' title='rustc_serialize::serialize::Encodable'>Encodable</a> for <a class='struct' href='moonlander_gp/struct.ScoreCard.html' title='moonlander_gp::ScoreCard'>ScoreCard</a>",];
+implementors["moonlander_gp"] = ["impl <a class=\"trait\" href=\"rustc_serialize/serialize/trait.Encodable.html\" title=\"trait rustc_serialize::serialize::Encodable\">Encodable</a> for <a class=\"struct\" href=\"moonlander_gp/struct.ScoreCard.html\" title=\"struct moonlander_gp::ScoreCard\">ScoreCard</a>",];
 
             if (window.register_implementors) {
                 window.register_implementors(implementors);
